e3ac14c2dfa31ee8f809ba1099407670725b0e72
Francisco Navarro <main@panch.local>
2019-06-15 17:32:32 -0700

    Add allowed hosts

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fn-weather-app.herokuapp.com']
 
 
 # Application definition
